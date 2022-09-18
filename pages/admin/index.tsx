f87a809731ba223af5b6ed128d065df8eae8f12e
Shashank Jarmale <shashankjarmale1@gmail.com>
2022-09-18 16:03:49 -0400

    Created admin page with ability to add events

---
+++ pages/admin/index.tsx
@@ -0,0 +1,58 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    // TODO: acquire this list of events from Firebase
+    const [communityEvent, setCommunityEvent] = useState("");
+    const [communityEventList, setCommunityEventList] = useState<string[]>([]);
+
+    const handleChangeEvent = (event: { target: { value: any } }) => {
+        setCommunityEvent(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (communityEvent !== "") {
+            setCommunityEventList([...communityEventList, communityEvent]);
+            setCommunityEvent("");
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1>Admin Page</h1>
+            <Link href='/'>Back to Home</Link>
+            <br />
+            <br />
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Add an event..."
+                    value={communityEvent}
+                    className='add-item-input'
+                    name="event"
+                    onChange={handleChangeEvent}
+                />
+
+                <button type="submit">Add</button>
+            </form>
+
+            <ul>
+                {communityEventList.map((communityEvent) => (
+                    <li key={communityEvent}>{communityEvent}</li>
+                ))}
+            </ul>
+
+            <br />
+            <br />
+
+            {/* TODO: Currently just goes back to home page */}
+            <Link href='/'>Log Out</Link>
+        </div>
+    )
+}
+
+export default Home

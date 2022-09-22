24a66257d027fb0ffce058442849a2bd11a7a2cf
Octillerysnacker <octillerysnacker@gmail.com>
2022-09-22 17:54:51 -0400

    added basic service types

---
+++ models/types.ts
@@ -0,0 +1,27 @@
+export interface Service {
+    income?: Range[];
+    language?: String[];
+    citizenship?: Citizenship[];
+    parentAge?: Range[];
+    childAge?: Range[];
+    family?: Family[];
+    employmentStatus?: EmploymentStatus[];
+    insurance?: Insurance[];
+    accessibility?: Accessibility[];
+}
+
+//Inclusive!!!
+export type Range = {
+    minimum: number;
+    maximum: number;
+}
+
+export type Citizenship = "Undocumented" | "Non-Immigrant" | "Resident" | "Citizen" | "Eligible Non-Citizen";
+
+export type Family = "Nuclear" | "Expecting" | "Extended" | "Single-Parent";
+
+export type EmploymentStatus = "Currently Working" | "Seeking Employment" | "Studying" | "Stay at Home"
+
+export type Insurance = "Uninsured" | "Private" | "Public"
+
+export type Accessibility = "Speech" | "Vision" | "Hearing" | "Mental" | "Digital Literacy"

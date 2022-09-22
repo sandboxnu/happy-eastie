37f6d76e3f02d75807b2656000091ef8c3440614
Simar Chadha <simar@Simars-MacBook-Air.local>
2022-09-22 19:00:57 -0400

    Added rest of quiz fields

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -10,13 +10,68 @@ const Home: NextPage = () => {
       <br />
       <br />
       <form action="/results" method="post">
-        <label>Annual Income:</label>
+        <label>Estimated Annual Income:</label>
         <br />
         <br />
         <input type="text" defaultValue="$" />
+        <br />
+        <br />
+        <label>Language</label>
+        <br />
+        <br />
+        <select name="languages" id="languages">
+          <option></option>
+          <option>English</option>
+          <option>Spanish</option>
+        </select>
+        <br />
+        <br />
+        <label>Citizenship</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Parent Age</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Child Age</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Family Type</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Employment Status</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Insurance Type</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
+        <label>Accesibility Needs</label>
+        <br />
+        <br />
+        <input type="text"/>
+        <br />
+        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
+    
   )
 }
 

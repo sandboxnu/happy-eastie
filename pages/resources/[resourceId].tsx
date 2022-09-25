b5db2cd0ec11a84a77a601fbde64184e7f0e6c97
cheopsrule <blake.heyman@gmail.com>
2022-09-25 17:01:14 -0400

    Resource ID work

--- pages/resources/[resourceId].tsx
+++ pages/resources/[resourceId].tsx
@@ -1,30 +1,29 @@
-import type { GetStaticPropsContext, NextPage } from 'next'
+import type { GetServerSidePropsContext, GetStaticProps, GetStaticPropsContext, NextPage } from 'next'
 import styles from '../../styles/Home.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link';
+import { Resource } from '../../models/types';
 
-type ResourceProps = {
-  employed: boolean
-}
-
-const Resource: NextPage = (props: ResourceProps) => {
+const ResourcePage: NextPage<Resource> = (resource: Resource) => {
     const router = useRouter();
+    console.log(resource)
 
   return (
     <div className={styles.container}>
-      <h1>Resource {props.employed}</h1>
+      <h1>Resource {resource.description}</h1>
       <Link href='/results'>Back to Results page</Link>
     </div>
   )
 }
 
-export async function getStaticProps({ params }: GetStaticPropsContext) {
-  const resourceData = fetch(`/api/resources/${params!.id}`);
-  return {
-    props: {
-      resourceData,
-    },
-  };
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  console.log(context.params?.resourceId)
+  const url = `http://localhost:3000/api/resources/${context.params?.resourceId}`
+  const res = await fetch(url)
+  console.log(res)
+  const resource: Resource = await res.json()
+
+  return resource
 }
 
-export default Resource
+export default ResourcePage

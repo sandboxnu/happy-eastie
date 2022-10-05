423e87d8bce0d3698159b927c6124bdfec7dfdc5
havngo <nvth110@gmail.com>
2022-10-05 19:54:36 -0400

    add components skeleton & info

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Navbar, Button, Link, Text, Card, Radio, Dropdown, Image } from "@nextui-org/react";
+import { Navbar, Dropdown, Image } from "@nextui-org/react";
 
 export default function Header() {
     return (
-        <Navbar variant={"sticky"}>
+        <Navbar maxWidth={"fluid"} variant={"sticky"}>
             <Navbar.Brand>
                 <Image width={"auto"} height={"auto"} src="/HappyEastie.svg"/>
             </Navbar.Brand>
-            <Navbar.Content>
+            <Navbar.Content enableCursorHighlight="true" gap={90}>
                 <Navbar.Link color="secondary" href="/quiz">Quiz</Navbar.Link>
                 <Navbar.Link color="secondary" href="#">Community Events</Navbar.Link>
                 <Navbar.Link color="secondary" href="#">Resources</Navbar.Link>
             </Navbar.Content>
             <Navbar.Content>
-                <Navbar.Item>
+                <Image width={20} height={20} src="/globe.svg"/>
                 <Dropdown>
                     <Dropdown.Button light>EN</Dropdown.Button>
                     <Dropdown.Menu aria-label="Static Actions">
@@ -21,7 +21,6 @@ export default function Header() {
                         <Dropdown.Item key="es">ES</Dropdown.Item>
                     </Dropdown.Menu>
                  </Dropdown>
-                </Navbar.Item>
             </Navbar.Content>
         </Navbar>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,52 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header'
+import { Grid, Image, Text, Button } from "@nextui-org/react";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header/>
+      <Grid.Container gap={2} justify="center" direction='column'>
+      <Grid>
+        <InfoComponent/>
+      </Grid>
+      <Grid>
+        <TrendingComponent/>
+      </Grid>
+      <Grid>
+        <DirectoryComponent/>
+      </Grid>
+      </Grid.Container>
     </div>
   )
+}
+
+function InfoComponent() {
+  return (
+    <Grid.Container gap={2} justify="center">
+    <Grid>
+      <Image alt="Default Image" src="/homeImage.svg" objectFit="scale-down"/>
+    </Grid>
+    <Grid direction="column">
+      <Text>Here to help find the resources for you!</Text>
+      <Text>Tap "Help My Search" to answer questions and get personalized resoure results</Text>
+      <Button shadow>Help My Search</Button>
+    </Grid>
+    </Grid.Container>
+  )
+}
+
+function TrendingComponent() {
+  return (
+    <div/>
+  );
+}
+
+function DirectoryComponent() {
+  return (
+    <div/>
+  );
 }
 
 export default Home

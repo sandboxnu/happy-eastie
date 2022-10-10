aeecb0ccf90d01587dfafe955dbff11210c94028
Octillerysnacker <octillerysnacker@gmail.com>
2022-10-10 13:56:07 -0400

    disabled navbar drop shadow

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,9 @@ import { Navbar, Dropdown, Image, useTheme } from "@nextui-org/react";
 
 export default function Header() {
     return (
-        <Navbar maxWidth={"fluid"} variant={"sticky"}>
+        <Navbar maxWidth={"fluid"} variant={"sticky"} disableShadow>
             <Navbar.Brand>
-                <Image width={"auto"} height={"auto"} src="/HappyEastie.svg"/>
+                <Image width={"auto"} height={"auto"} src="/HappyEastie.svg" />
             </Navbar.Brand>
             <Navbar.Content enableCursorHighlight="true" gap={90}>
                 <Navbar.Link className="navbar-content" href="/quiz">Quiz</Navbar.Link>
@@ -15,14 +15,14 @@ export default function Header() {
             <Navbar.Content>
                 <Dropdown>
                     <Dropdown.Button light>
-                        <Image width={20} height={20} style={{paddingRight: 4}} src="/globe.svg"/>
+                        <Image width={20} height={20} style={{ paddingRight: 4 }} src="/globe.svg" />
                         EN
                     </Dropdown.Button>
                     <Dropdown.Menu aria-label="Static Actions">
                         <Dropdown.Item key="fr">FR</Dropdown.Item>
                         <Dropdown.Item key="es">ES</Dropdown.Item>
                     </Dropdown.Menu>
-                 </Dropdown>
+                </Dropdown>
             </Navbar.Content>
         </Navbar>
     )

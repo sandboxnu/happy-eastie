92087b22a419af0028903bfd5d0d829c812b68ec
Simar Chadha <simar@Simars-MacBook-Air.local>
2022-10-17 22:44:41 -0400

    Removed reference to themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,12 @@ import { AppContext } from '../context/context'
 import { useState } from 'react'
 import { AES } from 'crypto-js'
 import { NextUIProvider } from '@nextui-org/react';
-import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // default is a hashed empty object, which the API will respond to with a list of all resources
   const [encryptedQuizResponse, changeEncryptedQuizResponse] = useState(AES.encrypt(JSON.stringify("{}"), "Secret Passphrase").toString())
   return (
-    <NextUIProvider theme={theme}>
+    <NextUIProvider>
       <div>
         <AppContext.Provider value={{ encryptedQuizResponse, changeEncryptedQuizResponse }}>
           <Component {...pageProps} />

9f5cc35df942a8453d402421fd856f810eaa1d32
cheopsrule <blake.heyman@gmail.com>
2022-10-19 19:06:08 -0400

    Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,31 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppContext } from '../context/context'
 import { useState } from 'react'
-import { AES } from 'crypto-js'
-import { NextUIProvider } from '@nextui-org/react';
-import { SWRConfig } from 'swr'
+import { createTheme, NextUIProvider, Text } from "@nextui-org/react"
+
+const theme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      primary: '#219EA4',
+      link: '#219EA4',
+
+      // you can also create your own color
+      brandBlue: '#22A6DD',
+      brandPurple: '#630DF0',
+      brandOrange: '#F0880D',
+      brandGreen: '#55C130',
+    },
+    space: {},
+    fonts: {}
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   // default is a hashed empty object, which the API will respond to with a list of all resources
   const [encryptedQuizResponse, changeEncryptedQuizResponse] = useState("")
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <div>
         <AppContext.Provider value={{ encryptedQuizResponse, changeEncryptedQuizResponse }}>
           <Component {...pageProps} />

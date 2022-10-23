be77b7ca43112c280d03b5ef935057c8cb52d3ea
cheopsrule <blake.heyman@gmail.com>
2022-10-23 15:01:13 -0400

    Delete old quiz

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,33 +1,15 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
-import React, { useContext } from 'react'
-import styles from '../../styles/Home.module.css'
-import { AES } from 'crypto-js'
-import { AppContext } from '../../context/context'
-import { QuizForm } from '../../components/quiz/QuizForm'
-import { useSWRConfig } from 'swr'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const Quiz: NextPage = () => {
   const router = useRouter()
-  const {cache} = useSWRConfig()
-  const quizState = useContext(AppContext)
 
-  const handleSubmit = (values: any) => {
-    const encrypted = AES.encrypt(JSON.stringify(values), "Secret Passphrase")
-    // clear old resources list from cache so cache never gets populated with too many lists
-    cache.delete('/api/resources')
-    quizState.changeEncryptedQuizResponse(encrypted.toString())
-    router.push('/quiz/results')
-  };
+  useEffect(() => {
+    router.push('quiz/1')
+  });
 
-  return (
-    <div className={styles.container}>
-      <h1>Quiz Page</h1>
-      <Link href='/'>Back to Home</Link>
-      <QuizForm onSubmitHandler={handleSubmit} initialValues={quizState.encryptedQuizResponse}/>
-    </div>
-  )
+  return <></>
 }
 
 export default Quiz

b88e5dff5e3047efde8c15e56e9fec8ee7f1feeb
cheopsrule <blake.heyman@gmail.com>
2022-10-30 17:01:15 -0400

    Initial resource page styling

---
+++ components/resources/ResourceHeader.tsx
@@ -0,0 +1,73 @@
+import { Resource } from "../../models/types";
+import { Button, Grid, Image } from "@nextui-org/react";
+import Tag from "../tag";
+import styles from "../../styles/resource.module.css";
+import { useRouter } from "next/router";
+
+interface ResourceHeaderProps {
+  resource: Resource;
+}
+
+export const ResourceHeader: React.FC<ResourceHeaderProps> = (props: ResourceHeaderProps) => {
+  const router = useRouter();
+
+  const phoneNumberClicked = () => {
+    router.push(`tel:${props.resource.phoneNumber}`);
+  };
+
+  const emailClicked = () => {
+    router.push(`mailto:${props.resource.email}`);
+  };
+
+  const shareClicked = () => {
+    const url = props.resource.url;
+    if (url) {
+      router.push(url);
+    }
+  };
+
+  return (
+    <div>
+      <Image src={props.resource.headerImageUrl} objectFit="cover" alt="Resource header image" />
+      <Grid.Container justify="center" gap={1}>
+        {props.resource.category?.map((c, i) => (
+          <Grid key={i} md={1}>
+            <Tag text={c} color="green" />
+          </Grid>
+        ))}
+      </Grid.Container>
+      <Grid.Container justify="center" gap={4}>
+        <Grid>
+          <Button
+            auto
+            color="primary"
+            icon={<Image src="/phonewhite.svg" objectFit="fill" alt="Call the organization" />}
+            rounded={true}
+            className={styles.button}
+            onClick={phoneNumberClicked}
+          ></Button>
+        </Grid>
+        <Grid>
+          <Button
+            auto
+            color="primary"
+            icon={<Image src="/email.svg" objectFit="fill" alt="Email the organization" />}
+            rounded={true}
+            className={styles.button}
+            onClick={emailClicked}
+          ></Button>
+        </Grid>
+        <Grid>
+          <Button
+            auto
+            color="primary"
+            icon={<Image src="/share.svg" objectFit="fill" alt="Share the organization" />}
+            rounded={true}
+            className={styles.button}
+            onClick={shareClicked}
+          ></Button>
+        </Grid>
+      </Grid.Container>
+    </div>
+  );
+};
--- pages/resources/[resourceId].tsx
+++ pages/resources/[resourceId].tsx
@@ -1,27 +1,26 @@
-import type { NextPage } from 'next'
-import styles from '../../styles/Home.module.css'
-import Link from 'next/link';
+import type { NextPage } from "next";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import { Image } from "@nextui-org/react";
 
-import { useResource } from '../../hooks/useResource';
-import { useRouter } from 'next/router';
-import { ResourceDisplay } from '../../components/resources/ResourceDisplay';
+import { useResource } from "../../hooks/useResource";
+import { useRouter } from "next/router";
+import { ResourceHeader } from "../../components/resources/ResourceHeader";
 
 const ResourcePage: NextPage = () => {
-  const router = useRouter()
-  const {resourceId} = router.query
-  const {resource, isLoading, error} = useResource(resourceId)
+  const router = useRouter();
+  const { resourceId } = router.query;
+  const { resource, isLoading, error } = useResource(resourceId);
 
-  if (error) return <div>{error.message}</div>
-  if (isLoading) return <div>loading...</div>
-  if (!resource) return <div>Internal server error: invalid resource loaded</div>
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+  if (!resource) return <div>Internal server error: invalid resource loaded</div>;
 
   return (
-    <div className={styles.container}>
-      <ResourceDisplay resource={resource}/>
-      <Link href='/resources'>Back to Results page</Link>
+    <div>
+      <ResourceHeader resource={resource} />
     </div>
-  )
-}
+  );
+};
 
-
-export default ResourcePage
+export default ResourcePage;

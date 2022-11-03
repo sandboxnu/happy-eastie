075f57cd5794aa71bd2b136886f4a141660f6f06
Blake Heyman <blake.heyman@gmail.com>
2022-11-03 19:05:13 -0400

    Image sizing

--- components/resources/ResourceDescription.tsx
+++ components/resources/ResourceDescription.tsx
@@ -12,26 +12,27 @@ export const ResourceDescription: React.FC<ResourceDescriptionProps> = (
 ) => {
   return (
     <Grid>
-      <Grid.Container justify="center" gap={8} wrap="nowrap">
-        <Grid>
-          <Text className={styles.descriptionText}>
-            {props.resource.description}
-          </Text>
-          <br />
-          <ReactMarkdown
-            className={styles.descriptionText}
-          >{`**Point of Contact:** ${props.resource.pointOfContact}`}</ReactMarkdown>
-          <ReactMarkdown
-            className={styles.descriptionText}
-          >{`**Waitlist:** ${props.resource.waitlist?.description}`}</ReactMarkdown>
+      <Grid.Container justify="center" gap={8}>
+        <Grid md={8}>
+          <div>
+            <Text className={styles.descriptionText}>
+              {props.resource.description}
+            </Text>
+            <br />
+            <ReactMarkdown
+              className={styles.descriptionText}
+            >{`**Point of Contact:** ${props.resource.pointOfContact}`}</ReactMarkdown>
+            <ReactMarkdown
+              className={styles.descriptionText}
+            >{`**Waitlist:** ${props.resource.waitlist?.description}`}</ReactMarkdown>
+          </div>
         </Grid>
-        <Grid>
+        <Grid md={4}>
           {/* TODO: put map of relevant locations here */}
           <Image
-            src={"https://hoodmaps.com/assets/maps/boston-neighborhood-map.png"}
+            src={"https://gisgeography.com/wp-content/uploads/2020/06/Boston-Road-Map.jpg"}
             alt="View the organization's locations"
-            width={400}
-            height={300}
+            autoResize
           />
         </Grid>
       </Grid.Container>

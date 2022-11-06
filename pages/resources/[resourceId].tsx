0475a6788f472768fed984c552691f3d30d718ba
Simar Chadha <simar@Simars-MacBook-Air.local>
2022-11-06 15:54:32 -0500

    Link hover fix. Added back button

--- pages/resources/[resourceId].tsx
+++ pages/resources/[resourceId].tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from "next";
 import { useResource } from "../../hooks/useResource";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { ResourceHeader } from "../../components/resources/ResourceHeader";
 import ReactMarkdown from "react-markdown";
-import { ResourceDescription } from "../../components/resources/ResourceDescription";
+import { Grid, Text } from "@nextui-org/react";
+import styles from "../../styles/resource.module.css"
 
 const ResourcePage: NextPage = () => {
   const router = useRouter();
   const { resourceId } = router.query;
   const { resource, isLoading, error } = useResource(resourceId);
+
+  function goBack() {
+    router.push("/");
+  }
 
   if (error) return <div>{error.message}</div>;
   if (isLoading) return <div>loading...</div>;
@@ -26,6 +31,13 @@ const ResourcePage: NextPage = () => {
         <Grid direction="column" css={{maxWidth: 440}}>
           <Text h3 css={{textAlign: "center"}}>How to Apply</Text>
           {howToApply && <ReactMarkdown children={howToApply} />}
+        </Grid>
+      </Grid.Container>
+      <Grid.Container justify="flex-start">
+        <Grid direction="row" md={1}>
+          <button className={styles.back} onClick={goBack}>
+                  Back
+          </button>
         </Grid>
       </Grid.Container>
     </Grid.Container>

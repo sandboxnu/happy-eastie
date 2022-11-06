fcf4f7c4148ed1f2c0b37e63957c5f3eeff60a16
Blake Heyman <blake.heyman@gmail.com>
2022-11-06 16:03:05 -0500

    Map embed

--- components/resources/ResourceDescription.tsx
+++ components/resources/ResourceDescription.tsx
@@ -10,10 +10,13 @@ interface ResourceDescriptionProps {
 export const ResourceDescription: React.FC<ResourceDescriptionProps> = (
   props: ResourceDescriptionProps
 ) => {
+  const mapRadius = 0.01; // Degrees latitude/longitude
+  const location = props.resource.location;
+  console.log(location);
   return (
     <Grid>
       <Grid.Container justify="center" gap={8}>
-        <Grid md={8}>
+        <Grid md={7}>
           <div>
             <Text className={styles.descriptionText}>
               {props.resource.description}
@@ -27,14 +30,23 @@ export const ResourceDescription: React.FC<ResourceDescriptionProps> = (
             >{`**Waitlist:** ${props.resource.waitlist?.description}`}</ReactMarkdown>
           </div>
         </Grid>
-        <Grid md={4}>
-          {/* TODO: put map of relevant locations here */}
-          <Image
-            src={"https://gisgeography.com/wp-content/uploads/2020/06/Boston-Road-Map.jpg"}
-            alt="View the organization's locations"
-            autoResize
-          />
-        </Grid>
+        {props.resource.location && (
+          <Grid md={4}>
+            {/* TODO: put map of relevant locations here */}
+            <iframe
+              width="850"
+              height="700"
+              src={`https://www.openstreetmap.org/export/embed.html?bbox=${
+                location!.longitude + mapRadius
+              }%2C${location!.latitude + mapRadius}%2C${
+                location!.longitude - mapRadius
+              }%2C${location!.latitude - mapRadius}&layer=mapnik&marker=${
+                location!.latitude
+              }%2C${location!.longitude}`}
+              style={{ border: "1px solid black" }}
+            ></iframe>
+          </Grid>
+        )}
       </Grid.Container>
     </Grid>
   );

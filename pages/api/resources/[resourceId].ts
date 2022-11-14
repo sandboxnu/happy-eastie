df86d59466936e78a738f1333fe3f838a025fa03
havngo <nvth110@gmail.com>
2022-11-14 18:27:30 -0500

    searchParam working

--- pages/api/resources/[resourceId].ts
+++ pages/api/resources/[resourceId].ts
@@ -1,26 +1,31 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type {Resource} from '../../../models/types'
-import mongoDbInteractor from '../../../firebase/mongoDbInteractor'
-import { WithId } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Resource } from "../../../models/types";
+import mongoDbInteractor from "../../../db/mongoDbInteractor";
+import { WithId } from "mongodb";
 
 export type ResourceResponse = {
-  data?: WithId<Resource>
-  error?: string
-}
-
+  data?: WithId<Resource>;
+  error?: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResourceResponse>
 ) {
-  const id = req.query['resourceId']
+  const id = req.query["resourceId"];
   if (!id || Array.isArray(id)) {
-    return res.status(401).json({"error": `Invalid value for required string query param eventId: received ${id}`})
-  } 
-  const resource = await getResource(id)
-  return resource ? res.status(200).json({data: resource}) : res.status(404).json({ error: `Resource ${id} not found`})
+    return res
+      .status(401)
+      .json({
+        error: `Invalid value for required string query param eventId: received ${id}`,
+      });
+  }
+  const resource = await getResource(id);
+  return resource
+    ? res.status(200).json({ data: resource })
+    : res.status(404).json({ error: `Resource ${id} not found` });
 }
 
-async function getResource(id: string) : Promise<WithId<Resource> | null> {
-    return await mongoDbInteractor.getDocument<Resource>('resources', id);
+async function getResource(id: string): Promise<WithId<Resource> | null> {
+  return await mongoDbInteractor.getDocument<Resource>("resources", id);
 }
--- pages/api/resources/index.ts
+++ pages/api/resources/index.ts
@@ -1,17 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Resource, ResourceCategory, ResourceSortingMethod, SurveyAnswers } from '../../../models/types'
-import {AES, enc} from 'crypto-js'
-import mongoDbInteractor from '../../../firebase/mongoDbInteractor'
-import { Filter, WithId } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  Resource,
+  ResourceCategory,
+  ResourceSortingMethod,
+  SurveyAnswers,
+} from "../../../models/types";
+import { AES, enc } from "crypto-js";
+import mongoDbInteractor, {
+  MongoDbInteractor,
+} from "../../../db/mongoDbInteractor";
+import { Filter, WithId } from "mongodb";
 
 export type ResourceData = {
-  requested: WithId<Resource>[],
-  additional: WithId<Resource>[]
-}
+  requested: WithId<Resource>[];
+  additional: WithId<Resource>[];
+};
 
 export type ResourcesResponse = {
-  data: ResourceData
-}
+  data: ResourceData;
+};
 
 // this endpoint supports two ways of getting resources:
 // 1. it expects a post request with body of structure {data: encryptedQuizResponses}
@@ -21,167 +28,193 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResourcesResponse | Array<Resource>>
 ) {
-  if (req.body['data']) {
+  if (req.body["data"]) {
     // TODO: error handling for invalid bodies sent
-    const encryptedFormData = req.body['data']
-    const formData : SurveyAnswers = JSON.parse(AES.decrypt(encryptedFormData, "Secret Passphrase").toString(enc.Utf8));
-    const resourceData = await getResources(formData)
-    res.status(200).json(resourceData)
-  } else if (req.body['searchParam']) {
-    const searchQuery = req.body['searchParam']
-    const resourceData = await getResourcesDirectory(req.body['searchParam'])
-    res.status(200).json([])
+    const encryptedFormData = req.body["data"];
+    const formData: SurveyAnswers = JSON.parse(
+      AES.decrypt(encryptedFormData, "Secret Passphrase").toString(enc.Utf8)
+    );
+    const resourceData = await getResources(formData);
+    res.status(200).json(resourceData);
+  } else if (req.body["searchParam"]) {
+    const searchQuery = req.body["searchParam"];
+    const resourceData = await getResourcesDirectory(searchQuery);
+    res.status(200).json(resourceData);
   } else {
-    const resourceData = await getAllResources()
-    res.status(200).json(resourceData)
+    const resourceData = await getAllResources();
+    res.status(200).json(resourceData);
   }
 }
 
-async function getAllResources() : Promise<ResourcesResponse> {
-  const requested = await mongoDbInteractor.getDocuments<Resource>('resources', {})
+async function getAllResources(): Promise<ResourcesResponse> {
+  const requested = await mongoDbInteractor.getDocuments<Resource>(
+    "resources",
+    {}
+  );
   return {
     data: {
       requested,
-      additional: []
-    }
-  }
+      additional: [],
+    },
+  };
 }
 
-async function getResources(answers: SurveyAnswers) : Promise<ResourcesResponse> {
-    const filter : Filter<Resource> = convertToFilter(answers)
-    let resources = await mongoDbInteractor.getDocuments<Resource>('resources', filter)
-    const requested : WithId<Resource>[] = []
-    const additional : WithId<Resource>[] = []
-    if (resources.length == 0) {
-      return {data: {
+async function getResources(
+  answers: SurveyAnswers
+): Promise<ResourcesResponse> {
+  const filter: Filter<Resource> = convertToFilter(answers);
+  let resources = await mongoDbInteractor.getDocuments<Resource>(
+    "resources",
+    filter
+  );
+  const requested: WithId<Resource>[] = [];
+  const additional: WithId<Resource>[] = [];
+  if (resources.length == 0) {
+    return {
+      data: {
         requested: [],
-        additional: []
-      }}
-    }
-    resources.reduce((prev: WithId<Resource>, curr: WithId<Resource>) => {
-      (curr.category && curr.category.some((c1: ResourceCategory) => answers.category.some((c2: ResourceCategory) => c1 === c2))) ? requested.push(curr) : additional.push(curr)
-      return curr
-    })
-    return {data: {
+        additional: [],
+      },
+    };
+  }
+  resources.reduce((prev: WithId<Resource>, curr: WithId<Resource>) => {
+    curr.category &&
+    curr.category.some((c1: ResourceCategory) =>
+      answers.category.some((c2: ResourceCategory) => c1 === c2)
+    )
+      ? requested.push(curr)
+      : additional.push(curr);
+    return curr;
+  });
+  return {
+    data: {
       requested,
-      additional
-    }}
+      additional,
+    },
+  };
 }
 
-function convertToFilter(answers: SurveyAnswers) : Filter<Resource> {
-  let filter : Filter<Resource>[] = []
+function convertToFilter(answers: SurveyAnswers): Filter<Resource> {
+  let filter: Filter<Resource>[] = [];
   if (answers.income) {
-    filter.push({"$or": [{minimumIncome: {"$exists": false}}, {minimumIncome: {"$lte": answers.income}}]})
-    filter.push({"$or": [{maximumIncome: {"$exists": false}}, {maximumIncome: {"$gte": answers.income}}]}) 
+    filter.push({
+      $or: [
+        { minimumIncome: { $exists: false } },
+        { minimumIncome: { $lte: answers.income } },
+      ],
+    });
+    filter.push({
+      $or: [
+        { maximumIncome: { $exists: false } },
+        { maximumIncome: { $gte: answers.income } },
+      ],
+    });
   }
   if (answers.language) {
-    filter.push({"$or": [{language: {"$exists": false}}, {language: {"$in": answers.language}}]})
+    filter.push({
+      $or: [
+        { language: { $exists: false } },
+        { language: { $in: answers.language } },
+      ],
+    });
   }
   if (answers.citizenship) {
-    filter.push({"$or": [{citizenship: {"$exists": false}}, {citizenship: answers.citizenship}]})
+    filter.push({
+      $or: [
+        { citizenship: { $exists: false } },
+        { citizenship: answers.citizenship },
+      ],
+    });
   }
   if (answers.parentAge) {
-    filter.push({"$or": [{minimumParentAge: {"$exists": false}}, {minimumParentAge: {"$lte": answers.parentAge}}]})
-    filter.push({"$or": [{maximumParentAge: {"$exists": false}}, {maximumParentAge: {"$gte": answers.parentAge}}]})
+    filter.push({
+      $or: [
+        { minimumParentAge: { $exists: false } },
+        { minimumParentAge: { $lte: answers.parentAge } },
+      ],
+    });
+    filter.push({
+      $or: [
+        { maximumParentAge: { $exists: false } },
+        { maximumParentAge: { $gte: answers.parentAge } },
+      ],
+    });
   }
   if (answers.childAge) {
-    filter.push({"$or": [{minimumChildAge: {"$exists": false}}, {minimumChildAge: {"$lte": answers.childAge}}]})
-    filter.push({"$or": [{maximumChildAge: {"$exists": false}}, {maximumChildAge: {"$gte": answers.childAge}}]})
+    filter.push({
+      $or: [
+        { minimumChildAge: { $exists: false } },
+        { minimumChildAge: { $lte: answers.childAge } },
+      ],
+    });
+    filter.push({
+      $or: [
+        { maximumChildAge: { $exists: false } },
+        { maximumChildAge: { $gte: answers.childAge } },
+      ],
+    });
   }
   if (answers.family) {
-    filter.push({"$or": [{family: {"$exists": false}}, {family: answers.family}]})
+    filter.push({
+      $or: [{ family: { $exists: false } }, { family: answers.family }],
+    });
   }
   if (answers.employmentStatus) {
-    filter.push({"$or": [{employmentStatus: {"$exists": false}}, {employmentStatus: answers.employmentStatus}]})
+    filter.push({
+      $or: [
+        { employmentStatus: { $exists: false } },
+        { employmentStatus: answers.employmentStatus },
+      ],
+    });
   }
   if (answers.insurance) {
-    filter.push({"$or": [{insurance: {"$exists": false}}, {insurance: answers.insurance}]})
+    filter.push({
+      $or: [
+        { insurance: { $exists: false } },
+        { insurance: answers.insurance },
+      ],
+    });
   }
   if (answers.accessibility) {
-    filter.push({"$or": [{accessibility: {"$exists": false}}, {accessibility: {"$in": answers.accessibility}}]})
+    filter.push({
+      $or: [
+        { accessibility: { $exists: false } },
+        { accessibility: { $in: answers.accessibility } },
+      ],
+    });
   }
   if (filter.length == 0) {
-    return {}
+    return {};
   }
-  return {"$and": filter} 
+  return { $and: filter };
 }
 
+async function getResourcesDirectory(
+  searchQuery: string
+): Promise<ResourcesResponse> {
+  let filter: Filter<Resource> = {
+    $or: [
+      { name: { $regex: searchQuery, $options: "i" } },
+      { description: { $regex: searchQuery, $options: "i" } },
+    ],
+  };
 
-async function getResourcesDirectory(searchQuery: string) : Promise<ResourcesResponse> {
-  let resources = await FirebaseInteractor.getCollectionData('resources', resourceConverter, [])
-  let requested = resources.filter((r : Resource) => matchesSearchQuery(searchQuery, r))
+  let resources = await mongoDbInteractor.getDocuments<Resource>(
+    "resources",
+    filter
+  );
   // TODO: eventually implement the filtering and sorting either by doing it using Mongo (if we
   // switch over, or implemenet the two functions below)
   // let requestedFiltered = requestedSearchResults.filter((r: Resource) => matchesFilters(filters, r))
   // let requestedSorted = sort(requestedFiltered, sortingMethod)
-  return {data: {
-    requested,
-    additional: []
-  }}
+  return {
+    data: {
+      requested: resources,
+      additional: [],
+    },
+  };
 }
 
-function matchesSearchQuery(searchQuery: string, r: Resource) {
-  return r.name.toLowerCase().includes(searchQuery.toLowerCase())
-  || r.description?.toLowerCase().includes(searchQuery.toLowerCase());
-}
+function matchesFilters(filters: ResourceCategory[], r: Resource) {}
 
-function matchesFilters(filters: ResourceCategory[], r: Resource) {
-
-}
-
-function sort(resources: Resource[], sortingMethod: ResourceSortingMethod) {
-
-}
-
-function matchesSurvey(answers: SurveyAnswers, r: Resource) {
-  return (!answers.income || !r.maximumIncome || answers.income < r.maximumIncome) 
-    && (!answers.income || !r.minimumIncome || answers.income > r.minimumIncome)
-    && (!answers.language || !r.language || r.language.some(l1 => answers.language?.some(l2 => l1 === l2))
-    && (!answers.citizenship || !r.citizenship || r.citizenship.some(c => c === answers.citizenship))
-    && (!answers.parentAge || !r.maximumParentAge || answers.parentAge < r.maximumParentAge) 
-    && (!answers.parentAge || !r.minimumParentAge || answers.parentAge > r.minimumParentAge) 
-    && (!answers.childAge || !r.maximumChildAge || answers.childAge < r.maximumChildAge) 
-    && (!answers.childAge || !r.minimumChildAge || answers.childAge > r.minimumChildAge)
-    && (!answers.family || !r.family || r.family.some(f => f === answers.family))
-    && (!answers.employmentStatus || !r.employmentStatus || r.employmentStatus.some(e => e === answers.employmentStatus))
-    && (!answers.insurance || !r.insurance || r.insurance.some(i => i === answers.insurance))
-    && (!answers.accessibility || !r.accessibility || r.accessibility.some(a1 => answers.accessibility?.some(a2 => a1 === a2))))  
-}
-
-/*
-Legacy function that makes a bunch of where queries on firebase b
-but apparently you can't have compound queries on different fields so r.i.p.
-function getWhereQueries(answers : SurveyAnswers) : WhereQuery[] {
-  const q : WhereQuery[] = []
-  if (answers.category) {
-    q.push({field: "category", comparison: 'array-contains-any', value: answers.category})
-  } 
-  if (answers.income) {
-    q.push({field: "maximumIncome", comparison: '<=', value: answers.income})
-    q.push({field: "minimumIncome", comparison: '>=', value: answers.income})
-  }
-  if (answers.language) {
-    q.push({field: "language", comparison: 'array-contains', value: answers.language})
-  }
-  if (answers.citizenship) {
-    q.push({field: "citizenship", comparison: 'array-contains', value: answers.citizenship})
-  } 
-  if (answers.parentAge) {
-    q.push({field: "minimumParentAge", comparison: '>=', value: answers.parentAge})
-    q.push({field: "maximumParentAge", comparison: '<=', value: answers.parentAge})
-  }
-  if (answers.family) {
-    q.push({field: "family", comparison: 'array-contains', value: answers.family})
-  }
-  if (answers.employmentStatus) {
-    q.push({field: "employmentStatus", comparison: 'array-contains', value: answers.employmentStatus})
-  }
-  if (answers.insurance) {
-    q.push({field: "insurance", comparison: 'array-contains', value: answers.insurance})
-  }
-  if (answers.accessibility) {
-    q.push({field: "accessibility", comparison: 'array-contains', value: answers.accessibility})
-  }
-  return q
-}
-*/
+function sort(resources: Resource[], sortingMethod: ResourceSortingMethod) {}

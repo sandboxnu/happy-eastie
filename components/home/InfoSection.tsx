11b266d0e6a1d0b7168cd753ea5ad0fd192f21aa
Octillerysnacker <octillerysnacker@gmail.com>
2023-01-09 19:07:26 -0500

    update link on infosection

--- components/home/InfoSection.tsx
+++ components/home/InfoSection.tsx
@@ -1,11 +1,12 @@
 import { Grid, Image, Text, Button, Spacer, Link } from "@nextui-org/react";
 import styles from "./Home.module.css"
 import NextLink from "next/link"
+import NextImage from "next/image"
 export default function InfoSection() {
     return (
       <Grid.Container gap={2} justify="center">
         <Grid xs={12} sm={5}>
-          <Image alt="Default Image" src="/homeImage2.svg" objectFit="scale-down" />
+          <Image alt="Default Image" src="/homeImage2.svg" objectFit="scale-down"/>
         </Grid>
         <Grid xs={12} sm={7} direction="column" alignItems="center">
           <Text className={styles.title}>Here to help find the resources for you!</Text>
@@ -16,7 +17,7 @@ export default function InfoSection() {
           <Spacer y={2} />
           <Button className={styles.button} shadow>
             <NextLink href="/quiz">
-                <Link >Help My Search</Link>
+                Help My Search
             </NextLink>
           </Button>
         </Grid>

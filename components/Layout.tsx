bb555ff72f536de2392327df9d3f395eb2eecaee
Octillerysnacker <octillerysnacker@gmail.com>
2023-01-13 16:58:52 -0500

    added directory page with filter siderbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from "react"
+import { CSSProperties, ReactNode } from "react"
 import Footer from "./footer/Footer"
 import Header from "./header/Header"
 import styles from "./Layout.module.css"
-const Layout = ({children}: {children: ReactNode}) => {
+const Layout = ({children, includePadding = true}: {children: ReactNode, includePadding?: boolean}) => {
+
+    const css : CSSProperties = includePadding? {padding: "0 2em"} : {}
+
     return (<div className={styles.container}>
         <Header/>
-        <main className={styles.content}>{children}</main>
+        <main className={styles.content} style={css}>{children}</main>
         <Footer/>
     </div>)
 }
---
+++ components/directory/sidebar/ResourceSearchBar.tsx
@@ -0,0 +1,22 @@
+import { FormElement, Input, Image } from "@nextui-org/react";
+import { ChangeEvent } from "react";
+
+interface ResourceSearchBarProps {
+    placeholder: string;
+    onChange: (e: ChangeEvent<FormElement>) => void
+}
+
+export const ResourceSearchBar: React.FC<ResourceSearchBarProps> = (props: ResourceSearchBarProps) => {
+    return (
+        <Input
+            size="xl"
+            aria-label="Search"
+            type="search"
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+            css={{ margin: "25px" }}
+            rounded
+            contentLeft={<Image alt="Search Resources" src="magnifierflat.svg"></Image>}
+        />
+    )
+}

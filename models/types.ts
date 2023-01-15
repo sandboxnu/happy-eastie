2d61b1a3f10f39a4bc2424f5bcef3e72254f2545
Octillerysnacker <octillerysnacker@gmail.com>
2023-01-15 03:58:07 -0500

    silly comment

--- models/types.ts
+++ models/types.ts
@@ -64,7 +64,7 @@ export interface SurveyAnswers {
 }
 
 /**
- * @deprecated Use ResourceData instead. This should be deleted after ticket #.
+ * deprecated Use ResourceData instead. This should be deleted after ticket #.
  */
 // export interface Service extends DocumentData {
 //     name: string;

c7c5f1d73c78007b04078ec25b7f1966dfe1d759
havngo <nvth110@gmail.com>
2023-02-15 17:41:31 -0500

    updating filter sidebar ui and types

--- components/directory/sidebar/FilterSidebar.tsx
+++ components/directory/sidebar/FilterSidebar.tsx
@@ -2,82 +2,95 @@ import React, { ChangeEvent, useEffect, useState } from "react";
 import { SidebarCategories } from "./SidebarCategories";
 import { SidebarStatus } from "./SidebarStatus";
 import { SidebarFamily } from "./SidebarFamily";
-import {
-  Accessibility,
-  Citizenship,
-  EmploymentStatus,
-  Family,
-  Insurance,
-  Language,
-  Resource,
-  ResourceCategory,
-  SurveyAnswers,
-} from "../../../models/types";
+import { Resource, SurveyAnswers } from "../../../models/types2";
 import { WithId } from "mongodb";
 import { AES, enc } from "crypto-js";
 import { ResourcesResponse } from "../../../pages/api/resources";
 import { ResourceSearchBar } from "./ResourceSearchBar";
 import { FormElement } from "@nextui-org/react";
+import {
+  Language,
+  ResourceCategory,
+  Accessibility,
+} from "../../../models/types";
 
-const SEARCH_PLACEHOLDER_TEXT = "Search Resources"
+const SEARCH_PLACEHOLDER_TEXT = "Search Resources";
 
 interface FilterSidebarProps {
   setResources(resources: WithId<Resource>[]): void;
-  filterValues: FilterValues
-  filterSetters: FilterSetters
+  filterValues: FilterValues;
+  filterSetters: FilterSetters;
 }
 
 interface FilterValues {
   categories: ResourceCategory[];
-      language: Language[],
-      insurance: Insurance | undefined
-      income: number | undefined
-      citizenship: Citizenship | undefined
-      employment: EmploymentStatus | undefined
-      accessibility: Accessibility[]
-      family: Family | undefined
-      searchQuery: string
-      parentAge: number | undefined
-      childAge: number | undefined
-  
+  language: Language[];
+  income: number | undefined;
+  householdMembers: number | undefined;
+  accessibility: Accessibility[];
+  searchQuery: string;
+  // insurance: Insurance | undefined
+  // citizenship: Citizenship | undefined
+  // employment: EmploymentStatus | undefined
+  // family: Family | undefined
+  // parentAge: number | undefined
+  // childAge: number | undefined
 }
-interface FilterSetters{
-  setCategories(s: ResourceCategory[]): void
-      setLanguage(s: Language[]): void
-      setIncome(s: number | undefined): void
-      setInsurance(s: Insurance | undefined): void
-      setCitizenship(s: Citizenship | undefined): void
-      setEmployment(s: EmploymentStatus | undefined): void
-      setAccessibility(s: Accessibility[]): void
-      setFamily(f: Family | undefined): void
-      setParentAge(n: number | undefined): void
-      setChildAge(n: number | undefined): void
-      setSearchQuery(s: string | undefined): void
-  }
-
+interface FilterSetters {
+  setCategories(s: ResourceCategory[]): void;
+  setLanguage(s: Language[]): void;
+  setIncome(s: number | undefined): void;
+  setHouseholdMembers(s: number | undefined): void;
+  setAccessibility(s: Accessibility[]): void;
+  setSearchQuery(s: string | undefined): void;
+  // setInsurance(s: Insurance | undefined): void
+  // setCitizenship(s: Citizenship | undefined): void
+  // setEmployment(s: EmploymentStatus | undefined): void
+  // setFamily(f: Family | undefined): void
+  // setParentAge(n: number | undefined): void
+  // setChildAge(n: number | undefined): void
+}
 
 export const FilterSidebar: React.FC<FilterSidebarProps> = (
   props: FilterSidebarProps
 ) => {
+  // const {categories, language, insurance, citizenship, employment, accessibility, family, searchQuery, parentAge, childAge, income} = props.filterValues
+  const {
+    categories,
+    language,
+    accessibility,
+    householdMembers,
+    searchQuery,
+    income,
+  } = props.filterValues;
+  // const {setAccessibility, setCategories, setChildAge, setCitizenship, setEmployment, setFamily, setInsurance, setLanguage, setParentAge, setSearchQuery, setIncome} = props.filterSetters
+  const {
+    setAccessibility,
+    setCategories,
+    setLanguage,
+    setSearchQuery,
+    setIncome,
+    setHouseholdMembers,
+  } = props.filterSetters;
 
-  const {categories, language, insurance, citizenship, employment, accessibility, family, searchQuery, parentAge, childAge, income} = props.filterValues
-  const {setAccessibility, setCategories, setChildAge, setCitizenship, setEmployment, setFamily, setInsurance, setLanguage, setParentAge, setSearchQuery, setIncome} = props.filterSetters
   // The resources after the filters have been applied (but before the search query)
-  const [filteredResources, setFilteredResources] = useState<WithId<Resource>[]>([])
+  const [filteredResources, setFilteredResources] = useState<
+    WithId<Resource>[]
+  >([]);
 
   useEffect(() => {
-
     const filters: SurveyAnswers = {
-      category: categories,
-      income,
+      categories: categories,
+      householdIncome: income ? income : 0,
+      householdMembers: householdMembers ? householdMembers : 1,
       language: language.length > 0 ? language : undefined,
-      citizenship,
-      parentAge,
-      childAge,
-      family,
-      employmentStatus: employment,
-      insurance: insurance,
       accessibility: accessibility.length > 0 ? accessibility : undefined,
+      // citizenship,
+      // parentAge,
+      // childAge,
+      // family,
+      // employmentStatus: employment,
+      // insurance: insurance,
     };
 
     const fetchFilteredResources = async () => {
@@ -104,21 +117,27 @@ export const FilterSidebar: React.FC<FilterSidebarProps> = (
   }, [
     categories,
     language,
-    insurance,
     income,
-    citizenship,
-    employment,
     accessibility,
-    family,
-    parentAge,
-    childAge,
+    householdMembers,
+    // insurance,
+    // citizenship,
+    // employment,
+    // family,
+    // parentAge,
+    // childAge,
   ]);
 
   useEffect(() => {
-    if (searchQuery === SEARCH_PLACEHOLDER_TEXT) return props.setResources(filteredResources)
-    const searchQueryAppliedResources = filteredResources.filter(r => r.name.toLowerCase().includes(searchQuery.toLowerCase()) || r.description?.toLowerCase().includes(searchQuery.toLowerCase()))
-    props.setResources(searchQueryAppliedResources)
-  }, [filteredResources, searchQuery])
+    if (searchQuery === SEARCH_PLACEHOLDER_TEXT)
+      return props.setResources(filteredResources);
+    const searchQueryAppliedResources = filteredResources.filter(
+      (r) =>
+        r.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        r.description?.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    props.setResources(searchQueryAppliedResources);
+  }, [filteredResources, searchQuery]);
 
   return (
     <>
@@ -128,26 +147,30 @@ export const FilterSidebar: React.FC<FilterSidebarProps> = (
           setSearchQuery(e.target.value);
         }}
       />
-      <SidebarCategories setCategories={setCategories} categories={categories}/>
+      <SidebarCategories
+        setCategories={setCategories}
+        categories={categories}
+      />
       <SidebarStatus
-      language={language}
+        language={language}
         setLanguage={setLanguage}
-        insurance={insurance}
-        setInsurance={setInsurance}
+        // insurance={insurance}
+        // setInsurance={setInsurance}
         setIncome={setIncome}
-        citizenship={citizenship}
-        setCitizenship={setCitizenship}
-        employment={employment}
-        setEmployment={setEmployment}
+        setMembers={setHouseholdMembers}
+        // citizenship={citizenship}
+        // setCitizenship={setCitizenship}
+        // employment={employment}
+        // setEmployment={setEmployment}
         accessibility={accessibility}
         setAccessibility={setAccessibility}
       />
-      <SidebarFamily
+      {/* <SidebarFamily
       family={family}
         setFamily={setFamily}
         setParentAge={setParentAge}
         setChildAge={setChildAge}
-      />
+      /> */}
     </>
   );
 };
--- components/directory/sidebar/SidebarStatus.tsx
+++ components/directory/sidebar/SidebarStatus.tsx
@@ -12,17 +12,18 @@ import {
 import ClearFieldsButton from "./ClearFieldsButton";
 
 interface SidebarStatusProps {
-  language: Language[]
-  setLanguage(s: Language[]): void
-  insurance: Insurance | undefined
-  setInsurance(s: Insurance | undefined): void
-  setIncome(n: number | undefined): void
-  citizenship: Citizenship | undefined
-  setCitizenship(s: Citizenship | undefined): void
-  employment: EmploymentStatus | undefined
-  setEmployment(s: EmploymentStatus | undefined): void
-  accessibility: Accessibility[]
-  setAccessibility(s: Accessibility[]): void
+  language: Language[];
+  setLanguage(s: Language[]): void;
+  // insurance: Insurance | undefined
+  // setInsurance(s: Insurance | undefined): void
+  setIncome(n: number | undefined): void;
+  setMembers(n: number | undefined) : void;
+  // citizenship: Citizenship | undefined
+  // setCitizenship(s: Citizenship | undefined): void
+  // employment: EmploymentStatus | undefined
+  // setEmployment(s: EmploymentStatus | undefined): void
+  accessibility: Accessibility[];
+  setAccessibility(s: Accessibility[]): void;
 }
 
 export const SidebarStatus: React.FC<SidebarStatusProps> = (
@@ -34,17 +35,22 @@ export const SidebarStatus: React.FC<SidebarStatusProps> = (
       <Spacer y={1} />
 
       <Text className={styles.sidebarSubCategory}>Language</Text>
-      <Checkbox.Group color="primary" className={styles.sidebarCheckboxGroup} onChange={e => props.setLanguage(e as Language[])} value={props.language}>
+      <Checkbox.Group
+        color="primary"
+        className={styles.sidebarCheckboxGroup}
+        onChange={(e) => props.setLanguage(e as Language[])}
+        value={props.language}
+      >
         {Object.values(Language).map((language) => (
           <Checkbox key={language} value={language} size={"xs"}>
             <Text className={styles.sidebarCheckboxText}>{language}</Text>
           </Checkbox>
         ))}
       </Checkbox.Group>
-      <ClearFieldsButton setField={props.setLanguage} clearedValue={[]}/>
+      <ClearFieldsButton setField={props.setLanguage} clearedValue={[]} />
       <Spacer y={1} />
 
-      <Text className={styles.sidebarSubCategory}>Insurance Type</Text>
+      {/* <Text className={styles.sidebarSubCategory}>Insurance Type</Text>
       <Radio.Group color="primary" className={styles.sidebarCheckboxGroup} onChange={e => props.setInsurance(e as Insurance)} value={props.insurance ?? ""}>
         {Object.values(Insurance).map((insuranceType) => (
           <Radio key={insuranceType} value={insuranceType} size={"xs"}>
@@ -53,18 +59,35 @@ export const SidebarStatus: React.FC<SidebarStatusProps> = (
         ))}
       </Radio.Group>
       <ClearFieldsButton setField={props.setInsurance} clearedValue={undefined}/>
+      <Spacer y={1} /> */}
+      <Text className={styles.sidebarSubCategory}>Total Household Member</Text>
+      <Input
+        clearable
+        bordered
+        className={styles.sidebarInputBox}
+        onChange={(e) => {
+          if (!e.target.value) return props.setMembers(undefined);
+          const num = parseInt(e.target.value);
+          if (isNaN(num)) return;
+          return props.setMembers(num);
+        }}
+      />
+      <Spacer y={1} />
+      <Text className={styles.sidebarSubCategory}>Total Annual Income</Text>
+      <Input
+        clearable
+        bordered
+        className={styles.sidebarInputBox}
+        onChange={(e) => {
+          if (!e.target.value) return props.setIncome(undefined);
+          const num = parseInt(e.target.value);
+          if (isNaN(num)) return;
+          return props.setIncome(num);
+        }}
+      />
       <Spacer y={1} />
 
-      <Text className={styles.sidebarSubCategory}>Annual Income</Text>
-      <Input clearable bordered className={styles.sidebarInputBox} onChange={e => {
-        if (!e.target.value) return props.setIncome(undefined)
-        const num = parseInt(e.target.value)
-        if (isNaN(num)) return
-        return props.setIncome(num)
-      }} />
-      <Spacer y={1} />
-
-      <Text className={styles.sidebarSubCategory}>Citizenship Status</Text>
+      {/* <Text className={styles.sidebarSubCategory}>Citizenship Status</Text>
       <Radio.Group color="primary" className={styles.sidebarCheckboxGroup} onChange={e => props.setCitizenship(e as Citizenship)} value={props.citizenship ?? ""}>
         {Object.values(Citizenship).map((citizenshipStatus) => (
           <Radio key={citizenshipStatus} value={citizenshipStatus} size={"xs"}>
@@ -73,9 +96,9 @@ export const SidebarStatus: React.FC<SidebarStatusProps> = (
         ))}
       </Radio.Group>
       <ClearFieldsButton clearedValue={undefined} setField={props.setCitizenship}/>
-      <Spacer y={1} />
+      <Spacer y={1} /> */}
 
-      <Text className={styles.sidebarSubCategory}>Employment Status</Text>
+      {/* <Text className={styles.sidebarSubCategory}>Employment Status</Text>
       <Radio.Group color="primary" className={styles.sidebarCheckboxGroup} onChange={e => props.setEmployment(e as EmploymentStatus)} value={props.employment ?? ""}>
         {Object.values(EmploymentStatus).map((employmentStatus) => (
           <Radio key={employmentStatus} value={employmentStatus} size={"xs"}>
@@ -84,17 +107,28 @@ export const SidebarStatus: React.FC<SidebarStatusProps> = (
         ))}
       </Radio.Group>
       <ClearFieldsButton setField={props.setEmployment} clearedValue={undefined}/>
-      <Spacer y={1} />
+      <Spacer y={1} /> */}
 
       <Text className={styles.sidebarSubCategory}>Accessibility Needs</Text>
-      <Checkbox.Group color="primary" className={styles.sidebarCheckboxGroup} onChange={e => props.setAccessibility(e as Accessibility[])} value={props.accessibility}>
+      <Checkbox.Group
+        color="primary"
+        className={styles.sidebarCheckboxGroup}
+        onChange={(e) => props.setAccessibility(e as Accessibility[])}
+        value={props.accessibility}
+      >
         {Object.values(Accessibility).map((accessibilityNeed) => (
-          <Checkbox key={accessibilityNeed} value={accessibilityNeed} size={"xs"}>
-            <Text className={styles.sidebarCheckboxText}>{accessibilityNeed}</Text>
+          <Checkbox
+            key={accessibilityNeed}
+            value={accessibilityNeed}
+            size={"xs"}
+          >
+            <Text className={styles.sidebarCheckboxText}>
+              {accessibilityNeed}
+            </Text>
           </Checkbox>
         ))}
       </Checkbox.Group>
-      <ClearFieldsButton setField={props.setAccessibility} clearedValue={[]}/>
+      <ClearFieldsButton setField={props.setAccessibility} clearedValue={[]} />
       <Spacer y={1} />
     </Grid>
   );

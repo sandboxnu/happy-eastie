cc32ca2b1ffa428ba5bccd95a894150e45c846dd
Shashank Jarmale <shashankjarmale1@gmail.com>
2023-03-01 22:16:45 -0500

    Minor changes

--- components/directory/sidebar/SidebarCategories.tsx
+++ components/directory/sidebar/SidebarCategories.tsx
@@ -15,18 +15,18 @@ export const SidebarCategories: React.FC<SidebarCategoriesProps> = (props: Sideb
             <Text className={styles.sidebarBanner}>Categories</Text>
             <Spacer y={1} />
             <Checkbox.Group
-                color="primary" 
+                color="primary"
                 className={styles.sidebarCheckboxGroup}
                 value={props.selectedCategories}
                 onChange={(e) => props.setSelectedCategories(e)}
             >
                 {props.categories.map(category => (
-                    <Checkbox css={{marginLeft: "10px", border: "3px solid var(--brand-primary)", borderRadius: "6px", width: "17px", height: "17px"}} key={category} value={category} size={"xs"}>
+                    <Checkbox css={{ marginLeft: "10px", border: "3px solid var(--brand-primary)", borderRadius: "6px", width: "17px", height: "17px" }} key={category} value={category} size={"xs"}>
                         <Text className={styles.sidebarCheckboxText}>{category}</Text>
                     </Checkbox>
                 ))}
             </Checkbox.Group>
-            <ClearFieldsButton setField={props.setSelectedCategories} clearedValue={[]}/>
+            <ClearFieldsButton setField={props.setSelectedCategories} clearedValue={[]} />
         </Grid>
     )
 }

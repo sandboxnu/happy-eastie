59379f50d696d2a8e58841ceff5278661d9323cd
Blake Heyman <blake.heyman@gmail.com>
2023-03-22 19:02:06 -0400

    WIP

--- pages/admin/resource/[resourceId].tsx
+++ pages/admin/resource/[resourceId].tsx
@@ -3,8 +3,9 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 // import { ResourceHeader } from "../../components/resources/ResourceHeader";
 import ReactMarkdown from "react-markdown";
-import { Image, Spacer, Text } from "@nextui-org/react";
+import { Button, Image, Row, Spacer, Text, Textarea } from "@nextui-org/react";
 import styles from "./[resourceId].module.css";
+import { useResource } from "../../../hooks/useResource";
 // import { ResourceDescription } from "../../components/resources/ResourceDescription";
 // import Loading from "../../components/Loading";
 // import Layout from "../../components/Layout";
@@ -12,7 +13,7 @@ import styles from "./[resourceId].module.css";
 const ResourcePageContent: NextPage = () => {
     const router = useRouter();
     const { resourceId } = router.query;
-    // const { resource, isLoading, error } = useResource(resourceId);
+    const { resource, isLoading, error } = useResource(resourceId);
   
     // if (error) return <div>{error.message}</div>;
     // if (isLoading) return <Loading/>;
@@ -22,26 +23,20 @@ const ResourcePageContent: NextPage = () => {
   
     return (
       <>
-        {resourceId}
-        {/* {resource.headerImageUrl &&
-          <Image src={resource.headerImageUrl} width="100%" height={254} objectFit="cover" alt="Resource header image"/>
-        }
-        <div style={{ margin: "0 40px" }}>
-          <ResourceHeader resource={resource} />
-          <Spacer y={2} />
-          <ResourceDescription resource={resource} />
-          <Spacer y={2} />
-          <div>
-            <Text h3 css={{ textAlign: "center" }}>
-              How to Apply
-            </Text>
-            {resource.eligibilityInfo && <ReactMarkdown>{resource.eligibilityInfo}</ReactMarkdown>}
-          </div>
-        </div>
-  
-        <button className={styles.back} onClick={() => router.back()}>
-          Back
-        </button> */}
+      <Text h3>Summary</Text>
+        <Textarea bordered borderWeight="light" fullWidth color="primary" />
+        <Spacer y={1} />
+        <Text h3>Description</Text>
+        <Textarea bordered borderWeight="light" fullWidth color="primary" />
+        <Spacer y={1} />
+        <Text h3>Contact</Text>
+        <Row>
+          <Image src="/email.svg" width="12px" height="9px" css={{backgroundColor: "$primary", py: 8, px: 6}}/>
+        </Row>
+        <Text h3>Website</Text>
+        <Text h3>Address</Text>
+        <Text h3>Categories</Text>
+        <Text h3>Keywords</Text>
       </>
     );
   };

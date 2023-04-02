6dfb250fe67e971657f02bd99ef5e018fca63599
Blake Heyman <blake.heyman@gmail.com>
2023-04-02 17:50:25 -0400

    Hide footer (#104)

--- components/header/HeaderCollapseMenu.tsx
+++ components/header/HeaderCollapseMenu.tsx
@@ -4,7 +4,7 @@ import NextLink from 'next/link'
 
 const HeaderCollapseMenu = ({ items }: { items: HeaderContent[] }) => {
   return (
-    <Navbar.Collapse style={{ opacity: 1, backgroundColor: "white", marginLeft: "20px" }} showIn="sm" >
+    <Navbar.Collapse style={{ opacity: 1, backgroundColor: "white", paddingLeft: 20}} showIn="sm">
       {items.map((item, index) => (
         <Navbar.CollapseItem key={item.title} css={{ fontSize: 20, fontWeight: "$semibold" }}>
           <NextLink

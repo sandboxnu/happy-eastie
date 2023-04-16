8661651175b119cc79a7d7515836ea5eed0898b4
Shashank Jarmale <shashankjarmale1@gmail.com>
2023-04-16 17:14:53 -0400

    Started implementing frontend for admin login page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -4,33 +4,36 @@ import {
   FormElement,
   Input,
   Spacer,
+  Grid,
+  Text
 } from "@nextui-org/react";
 import { ChangeEvent, useState } from "react";
-import styles from "../../components/quiz/Quiz.module.css";
+import quizStyles from "../../components/quiz/Quiz.module.css";
+import styles from "./admin.module.css";
 import CryptoJS from "crypto-js";
 
 import { useRouter } from "next/router";
 import { NORMAL_IRON_OPTION } from "../../models/constants";
-import { withIronSessionSsr } from "iron-session/next";
+// import { withIronSessionSsr } from "iron-session/next";
 
-export const getServerSideProps = withIronSessionSsr(
-  async function getServerSideProps({req}) {
-    const user = req.session.user;
+// export const getServerSideProps = withIronSessionSsr(
+//   async function getServerSideProps({req}) {
+//     const user = req.session.user;
 
-    if (user &&  user.isAdmin) {
-      return {
-        redirect: {
-          destination: '/admin/dashboard',
-          permanent: false,
-        }
-      };
-    }
-    else {
-      return {
-        props: {}
-      }
-    }
-  }, NORMAL_IRON_OPTION)
+//     if (user &&  user.isAdmin) {
+//       return {
+//         redirect: {
+//           destination: '/admin/dashboard',
+//           permanent: false,
+//         }
+//       };
+//     }
+//     else {
+//       return {
+//         props: {}
+//       }
+//     }
+//   }, NORMAL_IRON_OPTION)
 
 const LogIn = () => {
   const [email, setEmail] = useState("");
@@ -38,7 +41,6 @@ const LogIn = () => {
   const [keepSignedIn, setKeepSignedIn] = useState(false);
   const [message, setMessage] = useState("");
   const router = useRouter();
-
 
   const onEmailChange = (e: ChangeEvent<FormElement>) => {
     setEmail(e.target.value);
@@ -48,7 +50,7 @@ const LogIn = () => {
     setPassword(e.target.value);
   };
 
-  const submit = async () => {
+  const handleSubmit = async () => {
     setMessage("");
     const hashedPassword = CryptoJS.SHA256(password).toString()
     const requestBody = JSON.stringify({
@@ -70,17 +72,71 @@ const LogIn = () => {
     }
   };
 
+  const handleForgotPassword = async () => {
+
+  }
+
   return (
-    <>
-      {" "}
-      <p>{message}</p>
-      <Input
-        clearable
-        bordered
-        labelPlaceholder="Name"
-        onChange={onEmailChange}
-      />
-      <Spacer y={2.5} />
+    <div className={styles.container}>
+      <Grid.Container gap={2} alignItems="center" direction="column">
+        <Grid>
+          {message &&
+            <div className={styles.errorMessageContainer}>
+              <p className={styles.errorMessage}>{message}</p>
+            </div>
+          }
+
+          <Spacer y={0.5} />
+
+          <Text className={styles.adminLoginText}>Administrator Login</Text>
+
+          <Spacer y={0.5} />
+
+          <Input
+            clearable
+            bordered
+            placeholder="Email"
+            onChange={onEmailChange}
+            className={styles.adminLoginInput}
+            size="md"
+            animated={false}
+          />
+          <Spacer y={0.5} />
+          <Input.Password
+            placeholder="Password"
+            bordered
+            onChange={onPasswordChange}
+            className={styles.adminLoginInput}
+            size="md"
+            animated={false}
+          />
+
+          <Spacer y={0.5} />
+
+          <Checkbox onChange={(checked) => setKeepSignedIn(checked)}>
+            Keep me signed in
+          </Checkbox>
+
+          <Button
+            id="continue"
+            className={quizStyles.continue}
+            type="submit"
+            onPress={handleSubmit}
+          >
+            Log In
+          </Button>
+
+          <Button
+            id="forgot-password"
+            className={styles.forgotPassword}
+            onPress={handleForgotPassword}
+          >
+            Forgot Password?
+          </Button>
+        </Grid>
+      </Grid.Container>
+
+      {/* <Spacer y={2.5} />
       <Input.Password
         labelPlaceholder="Password"
         bordered
@@ -92,13 +148,13 @@ const LogIn = () => {
       </Checkbox>
       <Button
         id="continue"
-        className={styles.continue}
+        className={quizStyles.continue}
         type="submit"
         onPress={submit}
       >
         Log In
-      </Button>
-    </>
+      </Button> */}
+    </div>
   );
 };
 
